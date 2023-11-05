a6636d415029e4f60dcfb8366dd022401d236a10
Rade <rade.ilijev.kgb@gmail.com>
2023-11-05 21:30:47 +0200

    test

--- packages/utils/@utils/enums.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.ValueReplacement = void 0;
-var ValueReplacement;
-(function (ValueReplacement) {
-    ValueReplacement[ValueReplacement["All"] = 0] = "All";
-    ValueReplacement[ValueReplacement["After"] = 1] = "After";
-    ValueReplacement[ValueReplacement["Before"] = 2] = "Before";
-})(ValueReplacement || (exports.ValueReplacement = ValueReplacement = {}));
