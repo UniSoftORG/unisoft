275ccd63c7965e99331be6c1f5ac0fdf9f5e60e8
Rade <rade.ilijev.kgb@gmail.com>
2023-11-14 09:05:05 +0200

    Uni lang

---
+++ packages/@types-unisoft/dist/interfaces.d.ts
@@ -0,0 +1,27 @@
+export interface Nested<T> {
+    [key: string]: T | Nested<T> | undefined;
+}
+export interface FilterOptions<KeyType extends string = string> {
+    initialProperties?: KeyType[];
+    additionalProperties?: KeyType[];
+    parentKey?: KeyType;
+}
+export interface Conditional {
+    condition: string;
+    value: string;
+}
+export interface ExtractOptions<KeyType extends string = string> {
+    additionalProps?: KeyType[];
+    recursiveKey?: string;
+}
+export interface BinaryExpression {
+    left: Expression;
+    operator: string;
+    right: Expression;
+}
+export interface TernaryExpression {
+    condition: Expression;
+    trueBranch: Expression;
+    falseBranch: Expression;
+}
+export type Expression = string | BinaryExpression | TernaryExpression;
